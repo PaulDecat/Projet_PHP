093893ef2568024f1c3a9a2825b8c8b9eb7e86aa
Paul Decatoire <decatopaul@gmail.com>
2025-02-24 19:11:19 +0100

    Reprise de noe pour front

--- Vougthplus/resources/views/hero/index.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- resources/views/hero/index.blade.php -->
-<h1>Liste des héros</h1>
-<ul>
-    @foreach ($heroes as $hero)
-        <li>{{ $hero->name }}</li>
-    @endforeach
-</ul>
--- Vougthplus/resources/views/hero/show.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- resources/views/hero/index.blade.php -->
-<h1>Liste des héros</h1>
-<ul>
-    @foreach($heroes as $hero)
-        <li>
-            <a href="{{ route('hero.show', $hero->id) }}">{{ $hero->name }}</a>
-        </li>
-    @endforeach
-</ul>
